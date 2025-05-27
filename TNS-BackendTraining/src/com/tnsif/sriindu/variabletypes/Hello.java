edc8552d8e938ea62684dc3427156d567a47e452
khaleel1911 <work.khaleel.dev@gmail.com>
2025-05-27 21:59:40 +0530

    Testing- cloned the repo

---
+++ TNS-BackendTraining/src/com/tnsif/sriindu/variabletypes/Hello.java
@@ -0,0 +1,9 @@
+package com.tnsif.sriindu.variabletypes;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello this is demo");	}
+
+}
